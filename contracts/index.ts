94924bef5a3735a4bffcc526a2a0acc998b7e68d
ajaykumar1007 <ajay.kumar3@antiersolutions.com>
2025-04-29 18:01:55 +0530

    file update

--- contracts/index.ts
+++ contracts/index.ts
@@ -1,34 +1,25 @@
-import { Chain, createPublicClient, http, Address, defineChain } from 'viem';
-import { privateKeyToAccount } from 'viem/accounts';
+import { createPublicClient, http, Address } from 'viem';
+import { generatePrivateKey, privateKeyToAccount } from 'viem/accounts';
 import { arbitrumSepolia } from 'viem/chains';
 import {
-  createTokenBridgeEnoughCustomFeeTokenAllowance,
-  createTokenBridgePrepareCustomFeeTokenApprovalTransactionRequest,
-  createTokenBridgePrepareTransactionRequest,
-  createTokenBridgePrepareTransactionReceipt,
+  prepareChainConfig,
+  createRollupPrepareDeploymentParamsConfig,
+  createRollup,
 } from '@arbitrum/orbit-sdk';
-import { sanitizePrivateKey } from '@arbitrum/orbit-sdk/utils';
+import { sanitizePrivateKey, generateChainId } from '@arbitrum/orbit-sdk/utils';
 import { config } from 'dotenv';
 config();
 
-function getBlockExplorerUrl(chain: Chain) {
-  return chain.blockExplorers?.default.url;
+function withFallbackPrivateKey(privateKey: string | undefined): `0x${string}` {
+  if (typeof privateKey === 'undefined' || privateKey === '') {
+    return generatePrivateKey();
+  }
+
+  return sanitizePrivateKey(privateKey);
 }
 
-if (typeof process.env.ROLLUP_ADDRESS === 'undefined') {
-  throw new Error(`Please provide the "ROLLUP_ADDRESS" environment variable`);
-}
-
-if (typeof process.env.ROLLUP_OWNER_PRIVATE_KEY === 'undefined') {
-  throw new Error(`Please provide the "ROLLUP_OWNER_PRIVATE_KEY" environment variable`);
-}
-
-if (typeof process.env.ORBIT_CHAIN_ID === 'undefined') {
-  throw new Error(`Please provide the "ORBIT_CHAIN_ID" environment variable`);
-}
-
-if (typeof process.env.ORBIT_CHAIN_RPC === 'undefined') {
-  throw new Error(`Please provide the "ORBIT_CHAIN_RPC" environment variable`);
+if (typeof process.env.DEPLOYER_PRIVATE_KEY === 'undefined') {
+  throw new Error(`Please provide the "DEPLOYER_PRIVATE_KEY" environment variable`);
 }
 
 if (typeof process.env.CUSTOM_FEE_TOKEN_ADDRESS === 'undefined') {
@@ -41,6 +32,14 @@ if (typeof process.env.PARENT_CHAIN_RPC === 'undefined' || process.env.PARENT_CH
   );
 }
 
+// load or generate a random batch poster account
+const batchPosterPrivateKey = withFallbackPrivateKey(process.env.BATCH_POSTER_PRIVATE_KEY);
+const batchPoster = privateKeyToAccount(batchPosterPrivateKey).address;
+
+// load or generate a random validator account
+const validatorPrivateKey = withFallbackPrivateKey(process.env.VALIDATOR_PRIVATE_KEY);
+const validator = privateKeyToAccount(validatorPrivateKey).address;
+
 // set the parent chain and create a public client for it
 const parentChain = arbitrumSepolia;
 const parentChainPublicClient = createPublicClient({
@@ -48,99 +47,41 @@ const parentChainPublicClient = createPublicClient({
   transport: http(process.env.PARENT_CHAIN_RPC),
 });
 
-// define chain config for the orbit chain
-const orbitChain = defineChain({
-  id: Number(process.env.ORBIT_CHAIN_ID),
-  network: 'Orbit chain',
-  name: 'orbit',
-  nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
-  rpcUrls: {
-    default: {
-      http: [process.env.ORBIT_CHAIN_RPC],
-    },
-    public: {
-      http: [process.env.ORBIT_CHAIN_RPC],
-    },
-  },
-  testnet: true,
-});
-const orbitChainPublicClient = createPublicClient({ chain: orbitChain, transport: http() });
-
-// load the rollup owner account
-const rollupOwner = privateKeyToAccount(sanitizePrivateKey(process.env.ROLLUP_OWNER_PRIVATE_KEY));
+// load the deployer account
+const deployer = privateKeyToAccount(sanitizePrivateKey(process.env.DEPLOYER_PRIVATE_KEY));
 
 async function main() {
+  // generate a random chain id
+  const chainId = generateChainId();
   // set the custom fee token
   const nativeToken: Address = process.env.CUSTOM_FEE_TOKEN_ADDRESS as `0x${string}`;
 
-  // prepare transaction to approve custom fee token spend
-  const allowanceParams = {
-    nativeToken,
-    owner: rollupOwner.address,
-    publicClient: parentChainPublicClient,
-  };
-  if (!(await createTokenBridgeEnoughCustomFeeTokenAllowance(allowanceParams))) {
-    const approvalTxRequest =
-      await createTokenBridgePrepareCustomFeeTokenApprovalTransactionRequest(allowanceParams);
+  const createRollupConfig = createRollupPrepareDeploymentParamsConfig(parentChainPublicClient, {
+    chainId: BigInt(chainId),
+    owner: deployer.address,
+    chainConfig: prepareChainConfig({
+      chainId,
+      arbitrum: {
+        InitialChainOwner: deployer.address,
+        DataAvailabilityCommittee: true,
+      },
+    }),
+  });
 
-    // sign and send the transaction
-    const approvalTxHash = await parentChainPublicClient.sendRawTransaction({
-      serializedTransaction: await rollupOwner.signTransaction(approvalTxRequest),
+  try {
+    await createRollup({
+      params: {
+        config: createRollupConfig,
+        batchPosters: [batchPoster],
+        validators: [validator],
+        nativeToken,
+      },
+      account: deployer,
+      parentChainPublicClient,
     });
-
-    // get the transaction receipt after waiting for the transaction to complete
-    const approvalTxReceipt = await parentChainPublicClient.waitForTransactionReceipt({
-      hash: approvalTxHash,
-    });
-
-    console.log(
-      `Tokens approved in ${getBlockExplorerUrl(parentChain)}/tx/${
-        approvalTxReceipt.transactionHash
-      }`,
-    );
+  } catch (error) {
+    console.error(`Rollup creation failed with error: ${error}`);
   }
-
-  // prepare the transaction for deploying the core contracts
-  const txRequest = await createTokenBridgePrepareTransactionRequest({
-    params: {
-      rollup: process.env.ROLLUP_ADDRESS as `0x${string}`,
-      rollupOwner: rollupOwner.address,
-    },
-    parentChainPublicClient,
-    orbitChainPublicClient,
-    account: rollupOwner.address,
-  });
-
-  // sign and send the transaction
-  console.log(`Deploying the TokenBridge...`);
-  const txHash = await parentChainPublicClient.sendRawTransaction({
-    serializedTransaction: await rollupOwner.signTransaction(txRequest),
-  });
-
-  // get the transaction receipt after waiting for the transaction to complete
-  const txReceipt = createTokenBridgePrepareTransactionReceipt(
-    await parentChainPublicClient.waitForTransactionReceipt({ hash: txHash }),
-  );
-  console.log(`Deployed in ${getBlockExplorerUrl(parentChain)}/tx/${txReceipt.transactionHash}`);
-
-  // wait for retryables to execute
-  console.log(`Waiting for retryable tickets to execute on the Orbit chain...`);
-  const orbitChainRetryableReceipts = await txReceipt.waitForRetryables({
-    orbitPublicClient: orbitChainPublicClient,
-  });
-  console.log(`Retryables executed`);
-  console.log(
-    `Transaction hash for first retryable is ${orbitChainRetryableReceipts[0].transactionHash}`,
-  );
-  console.log(
-    `Transaction hash for second retryable is ${orbitChainRetryableReceipts[1].transactionHash}`,
-  );
-
-  // fetching the TokenBridge contracts
-  const tokenBridgeContracts = await txReceipt.getTokenBridgeContracts({
-    parentChainPublicClient,
-  });
-  console.log(`TokenBridge contracts:`, tokenBridgeContracts);
 }
 
 main();
